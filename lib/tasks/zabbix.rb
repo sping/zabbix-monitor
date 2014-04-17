33bdc0b90caba3b0018278400966428c04e5452e
Robert Jan de Gelder <r.degelder@sping.nl>
2014-04-17 17:01:46 +0200

    Fixed typo

--- lib/tasks/zabbix.rb
+++ lib/tasks/zabbix.rb
@@ -8,7 +8,7 @@ namespace :zabbix do
   end
 
   desc 'Collect data for Zabbix every minute'
-  task :collect_once => :environment do
+  task :collect_data => :environment do
     Rufus::Scheduler.new.tap do |scheduler|
       scheduler.every '1m' do
         ActiveRecord::Base.connection_pool.with_connection do
