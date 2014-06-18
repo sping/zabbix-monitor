6017aa834284adcefa49ae12fb8f37bede284367
Robert Jan de Gelder <r.degelder@sping.nl>
2014-06-18 12:24:52 +0200

    Fixed config spec

--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -6,25 +6,11 @@ describe Zabbix::Config do
   let(:config) { Zabbix::Config.new }
 
   describe '#mode' do
-    it 'allows setting mode to push' do
-      expect(logger).to receive(:info).with(/setting mode to 'push'/)
-      config.mode = :push
-    end
-    it 'allows setting mode to file' do
-      expect(logger).to receive(:info).with(/setting mode to 'file'/)
-      config.mode = :file
-    end
-    it 'allows setting mode to stdout' do
-      expect(logger).to receive(:info).with(/setting mode to 'stdout'/)
-      config.mode = :stdout
-    end
     it 'raises an unsupported mode for a invalid mode' do
-      expect(logger).to receive(:error).with(/failed setting mode to 'wrong'/)
       expect { config.mode = :wrong }
         .to raise_error(StandardError, 'Unsupported mode: wrong')
     end
     it 'raises an unsupported mode for a empty mode' do
-      expect(logger).to receive(:error).with(/failed setting mode to ''/)
       expect { config.mode = '' }
         .to raise_error(StandardError, 'Unsupported mode: ')
     end
